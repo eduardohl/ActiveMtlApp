766d49ee6a9fa917b1fd4975f9305c32d5ccd7b7
fGINIOUX <frederic.ginioux@hotmail.fr>
2013-10-16 20:53:01 -0400

    Delete title in home feed

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,6 @@ module.exports = function(app){
         var next = function(err, data){
 
             if(!err){
-                data.title = "this is a generic title";
                 res.render('index', data);
             } else {
                 res.render('404', { errorMessage: 'Express' });
