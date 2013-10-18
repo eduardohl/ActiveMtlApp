c3e5d83182302d60464bde1c47afd349fbfdb203
Frederic GINIOUX <frederic.ginioux@hotmail.fr>
2013-10-17 20:56:33 -0400

    Remove test deployement heroku

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,6 @@ module.exports = function(app){
 
             if(!err){
                 data.context = "home";
-                console.log(data);
                 res.render('index', data);
             } else {
                 res.render('404', { errorMessage: 'Express' });
