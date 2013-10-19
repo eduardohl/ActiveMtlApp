a051a6290e5235e46a3a7e8b3cdf25de7c4c79c8
unknown <mathieu.corriveau@ML-14144.nurun.com>
2013-10-19 13:21:04 -0400

    Change button color according to type of page

--- routes/index.js
+++ routes/index.js
@@ -125,7 +125,7 @@ module.exports = function(app){
                         data.location = null;
                     }
                 }
-
+                
                 res.render('detail', data);
             } else {
                 res.render('404', { errorMessage: 'Express' });
